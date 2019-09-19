7fe9fe5549973e07a076828bbf528095924d366d
C.DeMaio <C.DeMaio@outlook.com>
2019-09-19 01:22:23 -0400

    Main Version

--- app/src/main/java/com/example/tutr/MainActivity.java
+++ app/src/main/java/com/example/tutr/MainActivity.java
@@ -1,14 +1,104 @@
 package com.example.tutr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText UEmail;
+    private EditText UPassword;
+
+    private Button Login;
+
+    private FirebaseAuth UAuth;
+
+    private FirebaseAuth.AuthStateListener UAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        UAuth = FirebaseAuth.getInstance();
+
+        UEmail = (EditText) findViewById(R.id.E_Email);
+        UPassword = (EditText) findViewById(R.id.E_Password);
+
+        Login = (Button) findViewById(R.id.Login);
+
+
+        UAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if (firebaseAuth.getCurrentUser() != null){
+                    //Change Activity
+                }
+
+            }
+        };
+
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SignIn();
+                //Change Activity
+
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+   protected void onStart(){
+        super.onStart();
+
+        UAuth.addAuthStateListener(UAuthListener);
+   }
+
+
+
+
+    private void SignIn (){
+
+        String Email = UEmail.getText().toString();
+        String Password = UPassword.getText().toString();
+
+        if (TextUtils.isEmpty(Email) || TextUtils.isEmpty(Password)){
+
+            Toast.makeText(MainActivity.this, "Please Enter Email & Password", Toast.LENGTH_LONG).show();
+
+        }else{
+            UAuth.signInWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+
+                    if (!task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Invalid Email & Password", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            });
+        }
+
+
+
     }
 }
